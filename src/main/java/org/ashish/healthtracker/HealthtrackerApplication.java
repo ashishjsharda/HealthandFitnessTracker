858f577c223a9cbd922fb5e77611818ad8741bf8
Ashish Sharda <ashishjsharda@gmail.com>
2024-09-09 07:22:29 -0400

    Initial commit - Health and Fitness Tracker setup

---
+++ src/main/java/org/ashish/healthtracker/HealthtrackerApplication.java
@@ -0,0 +1,13 @@
+package org.ashish.healthtracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthtrackerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HealthtrackerApplication.class, args);
+    }
+
+}
