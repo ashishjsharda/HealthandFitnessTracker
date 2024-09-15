36b358460f4256324f622fa29faa02ea2f07e479
Ashish Sharda <ashishjsharda@gmail.com>
2024-09-15 09:04:05 -0400

    Add config,repository,service

---
+++ src/main/java/org/ashish/healthtracker/service/UserService.java
@@ -0,0 +1,27 @@
+package org.ashish.healthtracker.service;
+
+import org.ashish.healthtracker.dto.UserDTO;
+import org.ashish.healthtracker.entity.User;
+import org.ashish.healthtracker.repository.UserRepository;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    public UserService(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    public User registerUser(UserDTO userDTO) {
+        User user = new User();
+        user.setName(userDTO.getName());
+        user.setEmail(userDTO.getEmail());
+        user.setPassword(passwordEncoder.encode(userDTO.getPassword()));
+        return userRepository.save(user);
+    }
+}
+
